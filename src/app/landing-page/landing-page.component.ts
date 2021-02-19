5e9f7e1a637f90afe9879725839e1c5e97ba3aa6
Khoi Bui <emquacau123472>
2021-02-19 18:47:50 -0500

    Fix Navbar

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import {Collapse} from 'bootstrap';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,9 +11,34 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('mainNav') mainNavRef!: ElementRef;
+  @ViewChild('navCollapse') navCollapseRef!: ElementRef;
+
+  onScroll$ = new Observable();
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    this.onScroll();
+  }
+
+  onScroll() {
+    this.onScroll$ = fromEvent(window, 'scroll').pipe(
+      tap(_ => this.updateEl())
+    );
+  }
+
+  updateEl() {
+    this.updateMainNav();
+  }
+
+  updateMainNav() {
+    const offsetTop = window.pageYOffset + this.mainNavRef.nativeElement.getBoundingClientRect().top;
+    offsetTop > 100 ? this.renderer.addClass(this.mainNavRef.nativeElement, 'navbar-shrink') : this.renderer.removeClass(this.mainNavRef.nativeElement, 'navbar-shrink');
+  }
+
+  navItemClick() {
+    new Collapse(this.navCollapseRef.nativeElement).toggle();
   }
 
 }
