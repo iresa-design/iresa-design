4ac3ce64c3cb7486d1ed3e3baae2de9bd24649a7
Khoi Bui <emquacau123472>
2021-03-21 07:53:23 -0400

    Portfolio

--- src/app/landing-page/portfolio/portfolio.component.ts
+++ src/app/landing-page/portfolio/portfolio.component.ts
@@ -8,6 +8,14 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  portfolioList = [
+    {src: 'assets/img/portfolio/cabin.png', alt: 'cabin'},
+    {src: 'assets/img/portfolio/cake.png', alt: 'cake'},
+    {src: 'assets/img/portfolio/circus.png', alt: 'circus'},
+    {src: 'assets/img/portfolio/game.png', alt: 'game'},
+    {src: 'assets/img/portfolio/safe.png', alt: 'safe'},
+    {src: 'assets/img/portfolio/submarine.png', alt: 'submarine'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
